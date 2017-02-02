ed4c13535b85a256ae224fd7a39ea5c808db011e
Jhunel <jhunel_2389@yahoo.com>
2017-02-02 14:06:52 +0800

    update
    update

--- resources/views/customer/includes/headerlogo.blade.php
+++ resources/views/customer/includes/headerlogo.blade.php
@@ -1,5 +1,5 @@
 					<div id="header_logo">
 						<a href="{{ URL::Route('cusIndex') }}" title="DJems">
-							<img class="logo img-responsive" src="{{env('FILE_PATH_CUSTOM')}}img/nlogo6.png" alt="Handmade" width="306" height="100">
+							<img class="logo img-responsive" src="{{env('FILE_PATH_CUSTOM')}}img/nlogo6.PNG" alt="Handmade" width="306" height="100">
 						</a>
 					</div>
